9fef8a6c62494d058372787c4ff9939611c65ed5
ManojaD2004 <manojadkc2004@gmail.com>
2024-01-30 18:59:41 +0530

    Final leanrnt CommonJS implementation!

---
+++ prj15/build/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// const hello = require("./hello");
+// const { a, b } = require("./hello");
+const hello = require("./hello");
+const { getArrayLength, maxInterval } = hello;
+console.log(hello);
---
+++ prj15/hello.ts
@@ -0,0 +1,8 @@
+const maxInterval = 12;
+function getArrayLength(arr: any[]) {
+  return arr.length;
+}
+export = {
+  getArrayLength,
+  maxInterval,
+};
